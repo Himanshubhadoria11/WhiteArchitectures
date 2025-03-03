9ab79d6fa8b8b0b9c4645747324092264bdd3b93
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2025-03-03 20:01:52 +0530

    c

--- client/src/component/About.jsx
+++ client/src/component/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     
     <div className="aboutimage">
-     <div className="text-container">
+     <div className="text-containe">
         <h6 className="light">Architecture is the learned game,correct and magnificent,</h6>
         <h6 className="light">of forms assembled in the light.So keeping this in mind we</h6>
         <h6 className="light">started with our work.We are a team of 5 Architects with a</h6>
